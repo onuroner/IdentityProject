6463a77ea65a8bc8c11dcd6b4b3f416adaae7248
Onur Öner <onuroner4054@gmail.com>
2024-04-02 00:34:18 +0300

    Add project files.

---
+++ IdentityProject/Migrations/20240401184323_mig_3.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace IdentityProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3396a6b0-7c4b-4a63-8652-5f0ce2bf3f9b", null, "admin", "admin" },
+                    { "7c169ead-4d94-4038-bef7-8d32ea510e13", null, "seller", "seller" },
+                    { "db053962-f7bd-46de-aeca-0954d32cd02a", null, "client", "client" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3396a6b0-7c4b-4a63-8652-5f0ce2bf3f9b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7c169ead-4d94-4038-bef7-8d32ea510e13");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "db053962-f7bd-46de-aeca-0954d32cd02a");
+        }
+    }
+}
